4ac12f740be13696408453b940ec22a41b41ecf4
zeromakerplus <zeromaker@foxmail.com>
2023-06-30 12:55:42 +0800

    #

--- wire_spectral_img.py
+++ wire_spectral_img.py
@@ -26,7 +26,7 @@ from modules import utils
 if __name__ == '__main__':
     nonlin = 'wire'            # type of nonlinearity, 'wire', 'siren', 'mfn', 'relu', 'posenc', 'gauss'
     niters = 2000               # Number of SGD iterations
-    learning_rate = 1e-3        # Learning rate. 
+    learning_rate = 2e-2        # Learning rate. 
     
     # WIRE works best at 5e-3 to 2e-2, Gauss and SIREN at 1e-3 - 2e-3,
     # MFN at 1e-2 - 5e-2, and positional encoding at 5e-4 to 1e-3 
